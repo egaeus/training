f3ad3c23ddb621d05d47b4856e61136a3dd20ef2
egaeus <asd@asd.com>
2020-05-22 12:08:00 -0500

    Kotlin

---
+++ java/src/practice/codeforces/CF1347A.kt
@@ -0,0 +1,17 @@
+package practice.codeforces
+
+/**
+ * @author egaeus
+ * @mail sebegaeusprogram@gmail.com
+ * @veredict Accepted
+ * @url <https://codeforces.com/contest/1347/problem/A>
+ * @category implementation
+ * @date 22/05/2020
+ **/
+
+fun main() {
+    val a = readLine()!!.toInt()
+    repeat(a) {
+        println(readLine()!!.split(" ").map { a -> a.toInt() }.sum())
+    }
+}
---
+++ java/src/practice/codeforces/CF1347B.kt
@@ -0,0 +1,30 @@
+package practice.codeforces
+
+/**
+ * @author egaeus
+ * @mail sebegaeusprogram@gmail.com
+ * @veredict Accepted
+ * @url <https://codeforces.com/contest/1347/problem/B>
+ * @category geom
+ * @date 22/05/2020
+ **/
+
+fun main() {
+    var N = readLine()!!.toInt()
+    repeat(N) {
+        var A = readLine()!!.split(" ").map { x -> x!!.toInt() }.sorted()
+        var B = readLine()!!.split(" ").map { x -> x!!.toInt() }.sorted()
+        var ws = false
+        repeat(2) { i ->
+            run {
+                repeat(2) { j ->
+                    run {
+                        if (A[i] == B[j] && A[(i + 1) % 2] + B[(j + 1) % 2] == A[i])
+                            ws = true
+                    }
+                }
+            }
+        }
+        println(if (ws) "Yes" else "No")
+    }
+}
